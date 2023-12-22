d236f770afb759e3811ffe1f98af82e28a960ce5
shoeb7558 <83645262+shoeb7558@users.noreply.github.com>
2023-12-22 20:42:23 +0530

    Update_2.1

--- src/App.js
+++ src/App.js
@@ -1,18 +1,26 @@
-import React, { useState } from 'react';
-
+import React, { useState, useEffect } from 'react';
 import AddUser from './components/Users/AddUser';
 import UsersList from './components/Users/UsersList';
 
 function App() {
   const [usersList, setUsersList] = useState([]);
 
-  const addUserHandler = (uName, uAge, ucollege) => {
-    setUsersList((prevUsersList) => {
-      return [
-        ...prevUsersList,
-        { name: uName, age: uAge, college:ucollege, id: Math.random().toString() },
-      ];
-    });
+  // Load user data from localStorage on component mount
+  useEffect(() => {
+    const storedUsers = JSON.parse(localStorage.getItem('users'));
+    if (storedUsers) {
+      setUsersList(storedUsers);
+    }
+  }, []);
+
+  const addUserHandler = (uName, uAge, uCollege) => {
+    const newUser = { name: uName, age: uAge, college: uCollege, id: Math.random().toString() };
+
+    // Update state
+    setUsersList((prevUsersList) => [...prevUsersList, newUser]);
+
+    // Update localStorage
+    localStorage.setItem('users', JSON.stringify([...usersList, newUser]));
   };
 
   return (
